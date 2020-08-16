38aee97a4266e697487de269ecb52462e216c2b3
Lais Fernandes Fialho <laisffn@hotmail.com>
2020-08-16 11:05:30 -0300

    Removing comment

--- RestWithASPNETUdemy 03 - Connecting To Database/RestWithASPNETUdemy/Startup.cs
+++ RestWithASPNETUdemy 03 - Connecting To Database/RestWithASPNETUdemy/Startup.cs
@@ -37,7 +37,6 @@ namespace RestWithASPNETUdemy
             //Dependency Injection
             services.AddScoped<IPersonService, PersonServiceImplementation>();
 
-            //MvcOptions.EnableEndpointRouting = false;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
