fb3ca27fd5a96b898276533f5a1e0c3956451827
Lais Fernandes Fialho <laisffn@hotmail.com>
2020-10-11 18:09:07 -0300

    Implementing HATEOAS

---
+++ RestWithASPNETUdemy 11 - HATEOAS/RestWithASPNETUdemy/Model/Base/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace RestWithASPNETUdemy.Model.Base
+{
+    /*Contrato entre os atributos
+     e a estrutura da tabela*/
+    //[DataContract] //ordem em que os atributos sao inicializados
+    public class BaseEntity
+    {
+
+        public long? Id { get; set; }
+    }
+}
---
+++ RestWithASPNETUdemy 11 - HATEOAS/RestWithASPNETUdemy/Service/Implementations/BookServiceImpl.cs
@@ -0,0 +1,50 @@
+﻿using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Repository.Generic;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Service.Implementations
+{
+    public class BookServiceImpl : IBookService
+    {
+        private readonly IRepository<Book> _repository;
+
+        public BookServiceImpl(IRepository<Book> repository)
+        {
+            _repository = repository;
+        }
+
+        // Metodo responsável por criar uma nova pessoa
+        // nesse momento adicionamos o objeto ao contexto
+        // e finalmente salvamos as mudanças no contexto
+        // na base de dados
+        public Book Create(Book book)
+        {
+            return _repository.Create(book);
+        }
+
+        // Método responsável por retornar uma pessoa
+        public Book FindById(long id)
+        {
+            return _repository.FindById(id);
+        }
+
+        // Método responsável por retornar todas as pessoas
+        public List<Book> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        // Método responsável por atualizar uma pessoa
+        public Book Update(Book book)
+        {
+            return _repository.Update(book);
+        }
+
+        // Método responsável por deletar
+        // uma pessoa a partir de um ID
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
