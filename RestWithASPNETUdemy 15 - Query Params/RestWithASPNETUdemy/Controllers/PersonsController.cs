700c0f187fe59d8e2e76e15224e8f20bac9fe14e
Lais Fernandes Fialho <laisffn@hotmail.com>
2020-10-26 19:47:18 -0300

    Implementing Query Params

---
+++ RestWithASPNETUdemy 15 - Query Params/RestWithASPNETUdemy/Controllers/PersonsController.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Microsoft.AspNetCore.Mvc;
+
+using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Service;
+
+using System.Collections.Generic;
+
+using Tapioca.HATEOAS;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    /* Mapeia as requisições de http://localhost:{porta}/api/persons/v1/
+    Por padrão o ASP.NET Core mapeia todas as classes que extendem Controller
+    pegando a primeira parte do nome da classe em lower case [Person]Controller
+    e expõe como endpoint REST
+    */
+    [ApiVersion("1")]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class PersonsController : ControllerBase
+    {
+        //Declaração do serviço usado
+        private readonly IPersonService _personService;
+
+        /* Injeção de uma instancia de IPersonService ao criar
+        uma instancia de PersonController */
+        public PersonsController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        // Configura o Swagger para a operação
+        // http://localhost:{porta}/api/persons/v1/
+        // [SwaggerResponse((202), Type = typeof(List<Person>))]
+        // determina o objeto de retorno em caso de sucesso List<Person>
+        // O [SwaggerResponse(XYZ)] define os códigos de retorno 204, 400 e 401
+        [HttpGet]
+        [SwaggerResponse(200, Type = typeof(List<PersonVO>))]
+        [SwaggerResponse(204)]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        [Authorize("Bearer")]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get()
+        {
+            return Ok(_personService.FindAll());
+        }
+
+        // Configura o Swagger para a operação
+        // http://localhost:{porta}/api/persons/v1/
+        // [SwaggerResponse((202), Type = typeof(List<Person>))]
+        // determina o objeto de retorno em caso de sucesso List<Person>
+        // O [SwaggerResponse(XYZ)] define os códigos de retorno 204, 400 e 401
+        [HttpGet("find-by-name")]
+        [SwaggerResponse(200, Type = typeof(List<PersonVO>))]
+        [SwaggerResponse(204)]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        [Authorize("Bearer")]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult GetByName([FromQuery] string firstName, [FromQuery] string lastName)
+        {
+            return Ok(_personService.FindByName(firstName, lastName));
+        }
+
+        // Configura o Swagger para a operação
+        // http://localhost:{porta}/api/persons/v1/{id}
+        // [SwaggerResponse((202), Type = typeof(Person))]
+        // determina o objeto de retorno em caso de sucesso Person
+        // O [SwaggerResponse(XYZ)] define os códigos de retorno 204, 400 e 401
+        [HttpGet("{id}")]
+        [SwaggerResponse(200, Type = typeof(PersonVO))]
+        [SwaggerResponse(204)]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        [Authorize("Bearer")]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get(int id)
+        {
+            var person = _personService.FindById(id);
+            if (person == null) return NotFound();
+            return Ok(person);
+        }
+
+        // Configura o Swagger para a operação
+        // http://localhost:{porta}/api/
+        // [SwaggerResponse((202), Type = typeof(Person))]
+        // determina o objeto de retorno em caso de sucesso Person
+        // O [SwaggerResponse(XYZ)] define os códigos de retorno 400 e 401
+        [HttpPost]
+        [SwaggerResponse(201, Type = typeof(PersonVO))]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        [Authorize("Bearer")]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Post([FromBody]PersonVO person)
+        {
+            if (person == null) return BadRequest();
+            return new ObjectResult(_personService.Create((person)));
+        }
+
+        // Configura o Swagger para a operação
+        // http://localhost:{porta}/api/persons/v1/
+        // determina o objeto de retorno em caso de sucesso Person
+        // O [SwaggerResponse(XYZ)] define os códigos de retorno 400 e 401
+        [HttpPut]
+        [SwaggerResponse(202, Type = typeof(PersonVO))]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        [Authorize("Bearer")]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Put([FromBody]PersonVO person)
+        {
+            if (person == null) return BadRequest();
+            var updatedPerson = _personService.Update(person);
+            if (updatedPerson == null) return BadRequest();
+            return new ObjectResult(updatedPerson);
+        }
+        
+        // Configura o Swagger para a operação
+        // http://localhost:{porta}/api/persons/v1/
+        // determina o objeto de retorno em caso de sucesso Person
+        // O [SwaggerResponse(XYZ)] define os códigos de retorno 400 e 401
+        [HttpPatch]
+        [SwaggerResponse(202, Type = typeof(PersonVO))]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        [Authorize("Bearer")]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Patch([FromBody]PersonVO person)
+        {
+            if (person == null) return BadRequest();
+            var updatedPerson = _personService.Update(person);
+            if (updatedPerson == null) return BadRequest();
+            return new ObjectResult(updatedPerson);
+        }
+
+        // Configura o Swagger para a operação
+        // http://localhost:{porta}/api/persons/v1/{id}
+        // O [SwaggerResponse(XYZ)] define os códigos de retorno 400 e 401
+        [HttpDelete("{id}")]
+        [SwaggerResponse(204)]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        [Authorize("Bearer")]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Delete(int id)
+        {
+            _personService.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ RestWithASPNETUdemy 15 - Query Params/RestWithASPNETUdemy/Service/IPersonService.cs
@@ -0,0 +1,15 @@
+﻿using RestWithASPNETUdemy.Data.VO;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Service
+{
+    public interface IPersonService
+    {
+        PersonVO Create(PersonVO person);
+        PersonVO FindById(long id);
+        List<PersonVO> FindAll();
+        List<PersonVO> FindByName(string firstName, string lastName);
+        PersonVO Update(PersonVO person);
+        void Delete(long id);
+    }
+}
