32b1916a8d2b83699579f3f712638bf0228290d0
Lais Fernandes Fialho <laisffn@hotmail.com>
2020-12-20 16:43:08 -0300

    Implementing SaveFileToDisk

--- RestWithASPNETUdemy 17 - Working with files/RestWithASPNETUdemy/Service/Implementations/FileService.cs
+++ RestWithASPNETUdemy 17 - Working with files/RestWithASPNETUdemy/Service/Implementations/FileService.cs
@@ -24,15 +24,35 @@ namespace RestWithASPNETUdemy.Service.Implementations
         {
             throw new NotImplementedException();
         }
+        public async Task<FileDetailVO> SaveFileToDisk(IFormFile file)
+        {
+            FileDetailVO fileDetail = new FileDetailVO();
+
+            var fileType = Path.GetExtension(file.FileName);
+            var baseUrl = _context.HttpContext.Request.Host;
+
+            if (fileType.ToLower() == ".pdf" || fileType.ToLower() == ".jpg" || fileType.ToLower() == ".png" || fileType.ToLower() == ".jpeg")
+            {
+                var docName = Path.GetFileName(file.FileName);
+                if (file != null && file.Length > 0)
+                {
+                    var destination = Path.Combine(_basePath, "", docName);
+                    fileDetail.DocName = docName;
+                    fileDetail.DocType = fileType;
+                    fileDetail.DocUrl = Path.Combine(baseUrl + "/api/file/v1" + fileDetail.DocName);
+
+                    //Grava no disco
+                    using var stream = new FileStream(destination, FileMode.Create);
+                    await file.CopyToAsync(stream);
+                }
+            }
+            return fileDetail;
+        }
 
         public Task<List<FileDetailVO>> SaveFilesToDisk(IList<IFormFile> file)
         {
             throw new NotImplementedException();
         }
 
-        public Task<FileDetailVO> SaveFileToDisk(IFormFile file)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
